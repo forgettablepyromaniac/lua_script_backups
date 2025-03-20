73afb36dc71ab6554c8776b68ab0f5a97a0ec862
forgettablepyromaniac <forgettablepyromaniac@gmail.com>
2025-03-20 17:39:40 +0000

    added time fix (and bug fix)

--- 1.0.3/theAllEncompassingFixScript.lua
+++ 1.0.3/theAllEncompassingFixScript.lua
@@ -1,5 +1,7 @@
 originalZoom = 0.9
 originalSpeed = 1
+vocalType = 0
+updateTime = true
 
 function onCreatePost()
     setProperty('showCombo', false)
@@ -13,6 +15,21 @@ function onCreatePost()
             vocalType = 1
         else
             vocalType = 0
+        end
+    end
+end
+
+function onUpdatePost(elapsed)
+    local curTime = getSongPosition()
+    if curTime > 0 and updateTime then
+        timeRemaining = (getProperty("game.songLength") - curTime)
+
+        if timeBarType == "Time Left" then
+            setTextString("timeTxt", callMethodFromClass('flixel.util.FlxStringUtil', 'formatTime', {(timeRemaining / 1000) / playbackRate}))
+        elseif timeBarType == "Time Elapsed" then
+            setTextString("timeTxt", callMethodFromClass('flixel.util.FlxStringUtil', 'formatTime', {(curTime / 1000) / playbackRate}))
+        else -- format time is a little bit finicky, sorry about the weird code lol -fp
+            updateTime = false -- dont use more ram then we have to lol
         end
     end
 end
